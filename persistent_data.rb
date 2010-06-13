8d8893f92a973ca11ed8b052d7f45488cb63ddd5
Simon Hafner <hafnersimon@gmail.com>
2010-06-13 12:33:57 +0200

    added PersistentData

---
+++ persistent_data.rb
@@ -0,0 +1,36 @@
+require 'pathname'
+require 'yaml'
+module PersistentData
+  include Kernel
+
+  FILE  = Pathname.new(ENV['XDG_CONFIG_HOME']) + 'subtle' + 'config.yaml'
+
+  class << self
+    attr_accessor :data
+  end
+
+  def self.save(data)
+    File.open(FILE, 'w') {|file| file.write(data.to_yaml)}
+  end
+
+  def self.load
+    YAML.load(File.read(FILE)) if FILE.exists?
+  end
+
+  on :start do
+    @@data = load
+  end
+
+  on :reload do
+    puts 'reload'
+    if load != @@data
+      load
+    else
+      save(@@data)
+    end
+  end
+
+  on :exit do
+    save(@@data)
+  end
+end
